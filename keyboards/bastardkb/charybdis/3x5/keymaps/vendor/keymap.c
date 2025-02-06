b39dd89c6ca48d09fb8933dfe90843452907dec2
Andrei Shumailov <gentoo1993@gmail.com>
2025-02-06 21:12:00 +0100

    update keymap

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
@@ -16,18 +16,23 @@
  */
 #include QMK_KEYBOARD_H
 
+#define TAPPING_TERM 175
+#define TAPPING_TERM_PER_KEY
+
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 #    include "timer.h"
 #endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
 enum charybdis_keymap_layers {
     LAYER_BASE = 0,
-    LAYER_FUNCTION,
-    LAYER_NAVIGATION,
-    LAYER_MEDIA,
-    LAYER_POINTER,
     LAYER_NUMERAL,
     LAYER_SYMBOLS,
+    LAYER_NAVIGATION,
+    LAYER_MOUSE,
+    LAYER_FUNCTION,
+    LAYER_MEDIA,
+    LAYER_SYSTEM,
+    LAYER_POINTER,
 };
 
 // Automatically enable sniping-mode on the pointer layer.
@@ -45,11 +50,18 @@ static uint16_t auto_pointer_layer_timer = 0;
 #    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
 #endif     // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
-#define ESC_MED LT(LAYER_MEDIA, KC_ESC)
+#define KC_B_MED LT(LAYER_MEDIA, KC_B)
 #define SPC_NAV LT(LAYER_NAVIGATION, KC_SPC)
-#define TAB_FUN LT(LAYER_FUNCTION, KC_TAB)
+#define TAB_MOU LT(LAYER_MOUSE, KC_TAB)
 #define ENT_SYM LT(LAYER_SYMBOLS, KC_ENT)
 #define BSP_NUM LT(LAYER_NUMERAL, KC_BSPC)
+#define KC_N_FUN LT(LAYER_FUNCTION, KC_N)
+#define KC_H_SYS LT(LAYER_SYSTEM, KC_H)
+#define KC_G_SYS LT(LAYER_SYSTEM, KC_G)
+#define KC_P_MIN MT(K_MINS, KC_G)
+#define KC_M_UND MT(LSFT(K_MINS), KC_M)
+#define KC_DOT_EXC MT(LSFT(K_1), KC_DOT)
+#define KC_SLSH_COLON MT(LSFT(KC_SCLN), KC_SLSH)
 #define _L_PTR(KC) LT(LAYER_POINTER, KC)
 
 #ifndef POINTING_DEVICE_ENABLE
@@ -61,16 +73,16 @@ static uint16_t auto_pointer_layer_timer = 0;
 
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
-#define LAYOUT_LAYER_BASE                                                                     \
-       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
-       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, \
-       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
-                      ESC_MED, SPC_NAV, TAB_FUN, ENT_SYM, BSP_NUM
+#define LAYOUT_LAYER_BASE                                                                                      \
+       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,       KC_U,      KC_I,     KC_O,       KC_P,          \
+       KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H_SYS,   KC_J,      KC_K,     KC_L,       KC_QUOT,       \
+       KC_Z,   KC_X,   KC_C,   KC_V,   KC_B_MED,   KC_N_FUN,   KC_M_UND,  KC_COMM,  KC_DOT_EXC, KC_SLSH_COLON, \
+                      ESC_MED, SPC_NAV, TAB_MOU, ENT_SYM, BSP_NUM
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define ______________HOME_ROW_GACS_L______________ KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX
-#define ______________HOME_ROW_GACS_R______________ XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI
+#define ______________HOME_ROW_GASC_L______________ KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX
+#define ______________HOME_ROW_GASC_R______________ XXXXXXX, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI
 
 /*
  * Layers used on the Charybdis Nano.
@@ -90,11 +102,29 @@ static uint16_t auto_pointer_layer_timer = 0;
  * column. App is on the tertiary thumb key and other thumb keys are duplicated
  * from the base layer to enable auto-repeat.
  */
-#define LAYOUT_LAYER_FUNCTION                                                                 \
-    _______________DEAD_HALF_ROW_______________, KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12, \
-    ______________HOME_ROW_GACS_L______________, KC_SCRL,   KC_F4,   KC_F5,   KC_F6,  KC_F11, \
-    _______________DEAD_HALF_ROW_______________, KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10, \
-                      XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX
+#define LAYOUT_LAYER_FUNCTION                                                          \
+    KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR, _______________DEAD_HALF_ROW_______________, \
+    KC_F11, KC_F4, KC_F5, KC_F6, KC_CAPS, ______________HOME_ROW_GASC_R______________, \
+    KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, _______________DEAD_HALF_ROW_______________, \
+                      KC_ESC, KC_ESC, KC_ESC, KC_ESC, KC_ESC
+
+/**
+ * \brief System layer.
+ */
+#define LAYOUT_LAYER_SYSTEM
+    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,  \
+    _______________DEAD_HALF_ROW_______________, _______________DEAD_HALF_ROW_______________, \
+    _______________DEAD_HALF_ROW_______________, _______________DEAD_HALF_ROW_______________, \
+                      KC_ESC, KC_DEL, KC_DEL, KC_ESC, KC_DEL
+
+/**
+ * \brief Mouse layer.
+ */
+#define LAYOUT_LAYER_MOUSE
+    _______________DEAD_HALF_ROW_______________, KC_REDO, KC_PASTE, KC_COPY,  KC_CUT,  KC_UNDO \
+    ______________HOME_ROW_GASC_L______________, MS_LEFT, MS_DOWN, MS_UP,  MS_RGHT,  KC_F11,   \
+    _______________DEAD_HALF_ROW_______________, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR,  MS_BTN3, \
+                      XXXXXXX, _______, _______, MS_BTN1, MS_BTN2
 
 /**
  * \brief Media layer.
@@ -102,16 +132,16 @@ static uint16_t auto_pointer_layer_timer = 0;
  * Tertiary left- and right-hand layer is media and RGB control.  This layer is
  * symmetrical to accomodate the left- and right-hand trackball.
  */
-#define LAYOUT_LAYER_MEDIA                                                                    \
-    XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, \
-    KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, \
-    XXXXXXX, XXXXXXX, XXXXXXX,  EE_CLR, QK_BOOT, QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, \
-                      _______, KC_MPLY, KC_MSTP, KC_MSTP, KC_MPLY
+#define LAYOUT_LAYER_MEDIA                                                                      \
+    _______________DEAD_HALF_ROW_______________, XXXXXXX, KC_BRIU, KC_BRIU, RGB_MOD, XXXXXXX,   \
+    _______________DEAD_HALF_ROW_______________, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MUTE,   \
+    _______________DEAD_HALF_ROW_______________, RGB_RMOD, RGB_MOD, XXXXXXX, RGB_LMOD, RGB_TOG, \
+                      KC_ESC, KC_ESC, KC_ESC, XXXXXXX, KC_MPLY
 
 /** \brief Mouse emulation and pointer functions. */
 #define LAYOUT_LAYER_POINTER                                                                  \
     QK_BOOT,  EE_CLR, XXXXXXX, DPI_MOD, S_D_MOD, S_D_MOD, DPI_MOD, XXXXXXX,  EE_CLR, QK_BOOT, \
-    ______________HOME_ROW_GACS_L______________, ______________HOME_ROW_GACS_R______________, \
+    ______________HOME_ROW_GASC_L______________, ______________HOME_ROW_GASC_R______________, \
     _______, DRGSCRL, SNIPING, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SNIPING, DRGSCRL, _______, \
                       KC_BTN2, KC_BTN1, KC_BTN3, KC_BTN3, KC_BTN1
 
@@ -124,10 +154,10 @@ static uint16_t auto_pointer_layer_timer = 0;
  * base layer to avoid having to layer change mid edit and to enable auto-repeat.
  */
 #define LAYOUT_LAYER_NAVIGATION                                                               \
-    _______________DEAD_HALF_ROW_______________, _______________DEAD_HALF_ROW_______________, \
-    ______________HOME_ROW_GACS_L______________, KC_CAPS, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, \
-    _______________DEAD_HALF_ROW_______________,  KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, \
-                      XXXXXXX, _______, XXXXXXX,  KC_ENT, KC_BSPC
+    _______________DEAD_HALF_ROW_______________, KC_REDO, KC_PASTE, KC_COPY, KC_CUT, KC_UNDO, \
+    ______________HOME_ROW_GASC_L______________, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_DEL,  \
+    _______________DEAD_HALF_ROW_______________, KC_INS, KC_PGDN, KC_PGUP, KC_HOME,  KC_END,  \
+                      XXXXXXX, _______, XXXXXXX, KC_ENT, KC_BSPC
 
 /**
  * \brief Numeral layout.
@@ -138,7 +168,7 @@ static uint16_t auto_pointer_layer_timer = 0;
  */
 #define LAYOUT_LAYER_NUMERAL                                                                  \
     KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, _______________DEAD_HALF_ROW_______________, \
-    KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, ______________HOME_ROW_GACS_R______________, \
+    KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, ______________HOME_ROW_GASC_R______________, \
      KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS, _______________DEAD_HALF_ROW_______________, \
                        KC_DOT,    KC_0, KC_MINS, XXXXXXX, _______
 
@@ -151,30 +181,30 @@ static uint16_t auto_pointer_layer_timer = 0;
  */
 #define LAYOUT_LAYER_SYMBOLS                                                                  \
     KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, _______________DEAD_HALF_ROW_______________, \
-    KC_COLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, ______________HOME_ROW_GACS_R______________, \
+    KC_COLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, ______________HOME_ROW_GASC_R______________, \
     KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, _______________DEAD_HALF_ROW_______________, \
                       KC_LPRN, KC_RPRN, KC_UNDS, _______, XXXXXXX
 
 /**
  * \brief Add Home Row mod to a layout.
  *
- * Expects a 10-key per row layout.  Adds support for GACS (Gui, Alt, Ctl, Shift)
+ * Expects a 10-key per row layout.  Adds support for GASC (Gui, Alt, Shift, Ctl)
  * home row.  The layout passed in parameter must contain at least 20 keycodes.
  *
  * This is meant to be used with `LAYER_ALPHAS_QWERTY` defined above, eg.:
  *
- *     HOME_ROW_MOD_GACS(LAYER_ALPHAS_QWERTY)
+ *     HOME_ROW_MOD_GASC(LAYER_ALPHAS_QWERTY)
  */
-#define _HOME_ROW_MOD_GACS(                                            \
+#define _HOME_ROW_MOD_GASC(                                            \
     L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
     L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
     ...)                                                               \
              L00,         L01,         L02,         L03,         L04,  \
              R05,         R06,         R07,         R08,         R09,  \
-      LGUI_T(L10), LALT_T(L11), LCTL_T(L12), LSFT_T(L13),        L14,  \
-             R15,  RSFT_T(R16), RCTL_T(R17), LALT_T(R18), RGUI_T(R19), \
+      LGUI_T(L10), LALT_T(L11), LSFT_T(L12), LCTL_T(L13),        L14,  \
+             R15,  RCTL_T(R16), RSFT_T(R17), LALT_T(R18), RGUI_T(R19), \
       __VA_ARGS__
-#define HOME_ROW_MOD_GACS(...) _HOME_ROW_MOD_GACS(__VA_ARGS__)
+#define HOME_ROW_MOD_GASC(...) _HOME_ROW_MOD_GASC(__VA_ARGS__)
 
 /**
  * \brief Add pointer layer keys to a layout.
@@ -204,7 +234,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_wrapper(
-    POINTER_MOD(HOME_ROW_MOD_GACS(LAYOUT_LAYER_BASE))
+    POINTER_MOD(HOME_ROW_MOD_GASC(LAYOUT_LAYER_BASE))
   ),
   [LAYER_FUNCTION] = LAYOUT_wrapper(LAYOUT_LAYER_FUNCTION),
   [LAYER_NAVIGATION] = LAYOUT_wrapper(LAYOUT_LAYER_NAVIGATION),
@@ -212,6 +242,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_NUMERAL] = LAYOUT_wrapper(LAYOUT_LAYER_NUMERAL),
   [LAYER_POINTER] = LAYOUT_wrapper(LAYOUT_LAYER_POINTER),
   [LAYER_SYMBOLS] = LAYOUT_wrapper(LAYOUT_LAYER_SYMBOLS),
+  [LAYER_SYSTEM] = LAYOUT_wrapper(LAYOUT_LAYER_SYSTEM),
+  [LAYER_MOUSE] = LAYOUT_wrapper(LAYOUT_LAYER_MOUSE),
 };
 // clang-format on
 
@@ -255,3 +287,26 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 // rgb_matrix.c.
 void rgb_matrix_update_pwm_buffers(void);
 #endif
+
+// Combos
+const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_P, COMBO_END};
+const uint16_t PROGMEM combo_esc_left[] = {KC_A, KC_S, COMBO_END};
+const uint16_t PROGMEM combo_esc_right[] = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_colon[] = {KC_L, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM combo_copy[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_paste[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM combo_cut[] = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM combo_lang_switch[] = {KC_T, KC_Y, COMBO_END};
+const uint16_t PROGMEM combo_caps[] = {KC_Z, KC_M, COMBO_END};
+
+combo_t key_combos[] = {
+    [0] = COMBO(combo_esc, KC_ESC),
+    [1] = COMBO(combo_esc_left, KC_ESC),
+    [2] = COMBO(combo_esc_right, KC_ESC),
+    [3] = COMBO(combo_colon, KC_COLN),
+    [4] = COMBO(combo_copy, KC_COPY),
+    [5] = COMBO(combo_paste, KC_PASTE),
+    [6] = COMBO(combo_cut, KC_CUT),
+    [7] = COMBO(combo_lang_switch, LCTL(KC_RSFT)),
+    [8] = COMBO(combo_caps, KC_CAPS)
+};
